18a5b2a4fc345423de61647fc488b7b5418f0f79
github username <siobhan.p.mahoney@gmail.com>
2017-11-28 10:30:07 -0500

    responses to questions 3

--- review-question-3.rb
+++ review-question-3.rb
@@ -1,6 +1,52 @@
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
 
+# OUR RESPONSES
+
+class User
+  attr_accessor :name, :photos
+
+  def initialize(name)
+    @name = name
+    @photos = []
+  end
+end
+
+class Photo
+  attr_reader :user, :comments
+
+  def initialize
+    @comments = []
+  end
+
+  def user=(user)
+    @user = user
+    @user.photos << self
+  end
+
+  def make_comment(text)
+    comment = Comment.new(text)
+    @comments << comment
+    comment.photo = self
+  end
+
+end
+
+class Comment
+  attr_accessor :text, :photo
+  @@all = []
+
+  def initialize(text)
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
+
+#########
 
 photo = Photo.new
 user = User.new("Sophie")
